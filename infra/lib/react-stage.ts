276e898c252a7d4ea234c25b3116eeb56b73047a
Brian Sunter <brian@briansunter.com>
2021-03-31 23:28:08 -0700

    manual

--- infra/lib/react-stage.ts
+++ infra/lib/react-stage.ts
@@ -15,7 +15,7 @@ export class ReactStage extends Stage {
   public readonly webappBucket: Bucket;
     constructor(scope: Construct, id: string, html: Artifact, staticAssets:Artifact, envName: string, props?: StageProps) {
         super(scope, id, props);
-        const reactStack = new ReactStack(this, 'React'envName, );
+        const reactStack = new ReactStack(this, 'React', envName );
         const webappBucket = reactStack.webappBucket;
       new S3DeployAction({
         actionName: "Static-Assets",
