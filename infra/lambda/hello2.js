15ae391dd55024295d5aa0cc6cfabc419e27973d
Brian Sunter <public@briansunter.com>
2021-04-03 21:17:53 -0700

    dynamo

--- infra/lambda/hello2.js
+++ infra/lambda/hello2.js
@@ -1,8 +1,15 @@
+const { DynamoDBClient, ListTablesCommand } = require("@aws-sdk/client-dynamodb");
+const client = new DynamoDBClient({ region: "us-east-1" });
+const params = {
+    TableName : process.env.TABLE_NAME
+
+};
 exports.handler = async (event) => {
     // TODO implement
+    const entries = await client.query(params)
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! 2' ),
+        body: JSON.stringify('Hello from Lambda! 2' + JSON.stringify(entries) ),
     };
     return response;
 }; 
--- infra/lib/lambda-stack.ts
+++ infra/lib/lambda-stack.ts
@@ -3,10 +3,17 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from '@aws-cdk/aws-apigateway';
 import {Bucket} from "@aws-cdk/aws-s3";
 import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
 export class LambdaStack extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, envName: string,  props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const table = new dynamodb.Table(this, 'DevEntriesTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
 
     const postFunction = new NodejsFunction(this, 'PostFunction', {
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -20,7 +27,12 @@ export class LambdaStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'handler',
       entry: 'lambda/hello2.js',
+      environment: {
+        TABLE_NAME: table.tableName
+      },
     });
+
+    table.grantReadData(getFunction);
     const api = new apigateway.RestApi(this, 'hello-api', { });
     const getIntegration = new apigateway.LambdaIntegration(getFunction);
 
