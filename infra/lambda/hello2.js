1be3e1e05442f8779659e8910c8f88a0451a1a0b
Brian Sunter <public@briansunter.com>
2021-04-03 22:29:44 -0700

    doc client

--- infra/lambda/hello2.js
+++ infra/lambda/hello2.js
@@ -1,16 +1,15 @@
-const AWS = require('aws-sdk')
-AWS.config.region = process.env.AWS_REGION
-const documentClient = new AWS.DynamoDB.DocumentClient()
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const documentClient = new DynamoDB.DocumentClient({region: "us-east-1"});
 const params = {
     TableName : process.env.TABLE_NAME
 
 };
 exports.handler = async (event) => {
     // TODO implement
-    const entries = await documentClient.scan(params)
+    const entries = await documentClient.scan(params).promise()
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! 2' + JSON.stringify(entries.Items) ),
+        body: JSON.stringify('Hello from Lambda! 2' + JSON.stringify(entries.Items)),
     };
     return response;
 }; 
