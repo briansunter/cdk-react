0dc303041644e2386e76e159238a0c4d3796eefa
Brian Sunter <public@briansunter.com>
2021-04-03 22:41:37 -0700

    doc client

--- infra/lambda/hello.js
+++ infra/lambda/hello.js
@@ -1,6 +1,20 @@
-import { v4 as uuidv4, v4 } from 'uuid';
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const documentClient = new DynamoDB.DocumentClient({region: "us-east-1"});
+import { v4 as uuidv4 } from 'uuid';
+
+const params = {
+    TableName : process.env.TABLE_NAME
+};
 exports.handler = async (event) => {
     // TODO implement
+
+    const entries = await documentClient.put({ 
+        Item: {
+          id: uuidv4(),
+          name: JSON.parse(event.body).name
+        },
+        TableName: process.env.TABLE_NAME
+      }).promise()
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!' + uuidv4()),
--- infra/lambda/hello2.js
+++ infra/lambda/hello2.js
@@ -2,10 +2,8 @@ const DynamoDB = require('aws-sdk/clients/dynamodb');
 const documentClient = new DynamoDB.DocumentClient({region: "us-east-1"});
 const params = {
     TableName : process.env.TABLE_NAME
-
 };
 exports.handler = async (event) => {
-    // TODO implement
     const entries = await documentClient.scan(params).promise()
     const response = {
         statusCode: 200,
